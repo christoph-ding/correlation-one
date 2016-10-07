841fa19daa129eb42f7aba8fb2aa75902452adb8
Pokegear <christoph.ding@gmail.com>
2016-10-06 22:17:34 -0400

    Finished second problem.

--- testCode.py
+++ testCode.py
@@ -12,12 +12,25 @@ def monthly_income_parser(x):
     month_match = 'Monthly gross income' in str(x)
     annual_match = 'Annual gross income' in str(x)
     if month_match:
-        return re.findall('\d+', x )
+        return float(('').join(re.findall('\d+', x )))
     elif annual_match:
-        return re.findall('\d+', x )
+        return float(('').join(re.findall('\d+', x ))) / 12
     else:
         return np.nan
 
 df["monthly_income"] =  df["AGENT_NOTES"].apply(monthly_income_parser)
 
-print(df)
+# Set to be equal to the lower bound of the credit score range. Datatype should be a float.
+df["credit_score"] = df["CREDIT_RANGE"].apply(lambda x: float(re.findall('\d+', x )[0]))
+
+# Set to be equal to the lower bound of the debt range. Datatype should be a float.
+df["debt"] = df["EXISTING_DEBT"].fillna('0').str.replace(',','').apply(lambda x: float(re.findall('\d+', x )[0]))
+
+# grouped_df has the (1) count and (2) average loan amount of grouping the records by OFFICE_LOCATION (first) and by CREDIT_RANGE (second)
+grouped_df  =  df.groupby(['OFFICE_LOCATION', 'CREDIT_RANGE']).agg([np.count_nonzero, np.mean])
+
+# print(df)
+print(grouped_df)
+
+
+
